e2b332d9813fa7648c3f885888f2d99d5e1e103a
sunjoolee <sheisthepoem@naver.com>
2023-05-28 18:37:42 +0900

    implementing game data manager, added kitty prefab

---
+++ Assets/SunJooFolder/GameDataManager/GameDataManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager gameDataManager;
+
+    //(1) Game Login/Logout Data
+    public bool isLoggedIn;
+    public string loggedInUserCode; 
+
+    //(2) LoggedIn Data
+    //<user code, user name> dictionary 
+    public Dictionary<string, string> userCodeNameDict;
+    
+    //(3) Carrot List Data
+    //carrot info struct
+    public struct CarrotInfo{
+        string carrotName;
+        bool isSold;
+    }
+
+    //<user code, user carrot info list> dictionary
+    public Dictionary<string, List<CarrotInfo>> userCodeCarrotListDict;
+
+   
+    void Awake(){
+         //singleton pattern
+        DontDestroyOnLoad(gameDataManager);
+
+            gameDataManager = this;
+            Debug.Log("GameDataManager>> gameDataManager returned");
+        }
+        if (gameDataManager != this){
+            Destroy(gameDataManager);
+            Debug.Log("GameDataManager>> gameDataManager destroyed");
+        }
+
+        //(1) init
+        isLoggedIn = false;
+        loggedInUserCode = 0;
+
+        //(2) init
+        userCodeNameDict.Add("0", "Visitor");
+        userCodeNameDict.Add("1111", "ChoiSH");
+        userCodeNameDict.Add("2222", "KimYW");
+        userCodeNameDict.Add("3333", "LeeSJ");
+
+        //(3) init
+        userCodeCarrotListDict.Add("0", List<CarrotInfo>());
+        userCodeCarrotListDict.Add("1111", List<CarrotInfo>());
+        userCodeCarrotListDict.Add("2222", List<CarrotInfo>());
+        userCodeCarrotListDict.Add("3333", List<CarrotInfo>());
+    }
+
+    //(1) called when login happened in LoginScene
+    void ManageLoginData(string inputLoginCode){
+        isLoggedIn = true;
+        loggedInUserCode = inputLoginCode;
+        Debug.Log("GameDataManager>> Managed Login Data. loggedInUserCode: "+loggedInUserCode);
+    }
+
+    //(1) called when logout happened in MyPageScene
+    void ManageLogoutData(){
+        isLoggedIn = false;
+        loggedInUserCode = "0";
+        Debug.Log("GameDataManager>> Managed Logout Data. loggedInUserCode: "+loggedInUserCode);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
--- Assets/SunJooFolder/MyPage/MyCarrotManager.cs
+++ Assets/SunJooFolder/MyPage/MyCarrotManager.cs
@@ -6,88 +6,24 @@ using TMPro;
 
 public class MyCarrotManager : MonoBehaviour
 {   
-    public Button carrotButton;
-    public Button hiddenButton;
-    public Button soldButton;
+    //public Button carrotButton;
+    //public Button soldButton;
 
     public GameObject carrotInfoTemplate;
 
     public GameObject carrotContent;
-    public GameObject hiddenContent;
     public GameObject soldContent;
 
-   //public Panel carrotPanel;
-    //public Panel hiddenPanel;
-    //public Panel soldPanel;
-
-
     void Awake(){
-        onMyCarrotButtonClicked();
-
         for(int i = 0; i<5; ++i){
             GameObject carrotInfo = (GameObject)Instantiate(carrotInfoTemplate);
             carrotInfo.transform.SetParent(carrotContent.transform);
         }
-        for(int i = 0; i<3; ++i){
-            GameObject carrotInfo = (GameObject)Instantiate(carrotInfoTemplate);
-            carrotInfo.transform.SetParent(hiddenContent.transform);
-        }
+
         for(int i = 0; i<2; ++i){
             GameObject carrotInfo = (GameObject)Instantiate(carrotInfoTemplate);
             carrotInfo.transform.SetParent(soldContent.transform);
         }
-    }
-
-    public void onMyCarrotButtonClicked(){
-        Debug.Log("carrotButton Clicked");
-        
-        changeColorDark(carrotButton);
-        changeColorCarrot(hiddenButton);
-        changeColorCarrot(soldButton);
-        // makeVisible(carrotPanel);
-        // makeInvisible(hiddenPanel);
-        // makeInvisible(soldPanel);
-    }
-
-    public void onMyHiddenButtonClicked(){
-        Debug.Log("hiddenButton Clicked");
-        
-        changeColorCarrot(carrotButton);
-        changeColorDark(hiddenButton);
-        changeColorCarrot(soldButton);
-        // makeInvisible(carrotPanel);
-        // makeVisible(hiddenPanel);
-        // makeInvisible(soldPanel);
-    }
-    
-    public void onMySoldButtonClicked(){
-        Debug.Log("soldButton Clicked");
-        
-        changeColorCarrot(carrotButton);
-        changeColorCarrot(hiddenButton);
-        changeColorDark(soldButton);
-        // makeInvisible(carrotPanel);
-        // makeInvisible(hiddenPanel);
-        // makeVisible(soldPanel);
-    }
-
-    
-    // void makeInvisible(Panel panel){
-    //     panel.transform.localScale= new Vector3(0,0,0);
-    // }
-    
-    // void makeVisible(Panel panel){
-    //     panel.transform.localScale= new Vector3(1,1,1);
-    // }
-
-    void changeColorCarrot(Button btn){
-        ColorBlock colorBlock = btn.colors;
-        colorBlock.normalColor = new Color32(255, 138, 61, 255);
-    }
-
-    void changeColorDark(Button btn){
-        ColorBlock colorBlock = btn.colors;
-        colorBlock.normalColor = new Color32(255, 100, 61, 255);
     }
 
 }
